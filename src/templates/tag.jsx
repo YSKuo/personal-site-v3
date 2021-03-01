6105b127ac705f0f724d64c19d456ff71f6ff080
YSkuo <crowley3141@hotmail.com>
2021-03-01 11:47:24 +0800

    set Tag page

--- src/templates/tag.jsx
+++ src/templates/tag.jsx
@@ -1,19 +1,38 @@
 import React from "react";
+import styled from "styled-components";
 import { Helmet } from "react-helmet";
 import { graphql } from "gatsby";
+import { Container, Grid, Button, IconButton, Hidden } from "@material-ui/core";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import FolderOutlinedIcon from "@material-ui/icons/FolderOutlined";
 import Layout from "../layout";
 import PostListing from "../components/PostListing/PostListing";
+import SEO from "../components/SEO/SEO";
 import config from "../../data/SiteConfig";
 
 export default function TagTemplate({ pageContext, data }) {
   const { tag } = pageContext;
   const postEdges = data.allMarkdownRemark.edges;
+
+  const TagContainer = styled.div`
+    margin-bottom: 3rem;
+  `;
+
   return (
     <Layout>
-      <div className="tag-container">
-        <Helmet title={`Posts tagged as "${tag}" | ${config.siteTitle}`} />
+      <Helmet title={`Posts tagged as "${tag}" | ${config.siteTitle}`} />
+      <SEO />
+      <Container maxWidth="md">
+        <TagContainer>
+          <Button variant="contained" disableElevation>
+            #{tag}
+          </Button>
+        </TagContainer>
+
         <PostListing postEdges={postEdges} />
-      </div>
+      </Container>
     </Layout>
   );
 }
@@ -40,6 +59,7 @@ export const pageQuery = graphql`
             tags
             cover
             date
+            category
           }
         }
       }
