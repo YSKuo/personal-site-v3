609decb7d6f8dcfe514f812ac326f852c4c42342
YSkuo <crowley3141@hotmail.com>
2021-03-01 18:14:08 +0800

    set About page

---
+++ src/components/Pages/AboutPage/About.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+import styled from "styled-components";
+import {
+  Container,
+  Toolbar,
+  Grid,
+  Typography,
+  Link,
+  Button,
+  IconButton,
+  Hidden,
+  Divider,
+} from "@material-ui/core";
+import {
+  Section,
+  SectionTitle,
+  SectionSubtitle,
+  P,
+  B,
+  A,
+} from "../../Section/Section";
+import { MEDIA_QUERY_SM } from "../../../constants/breakpoint";
+import StyledDivider from "../../StyledDivider";
+
+function About({ config }) {
+  const {
+    userName,
+    userEmail,
+    userResume,
+    userLocation,
+    userLinks,
+    userSkills,
+  } = config;
+
+  const findUserLinks = (target) => {
+    return userLinks.find(
+      (item) => item.label.toLowerCase() === target.toLowerCase()
+    );
+  };
+
+  const linkedinInfo = findUserLinks("linkedin");
+  const instagramInfo = findUserLinks("instagram");
+
+  return (
+    <Container>
+      <Section>
+        <SectionTitle>About Me</SectionTitle>
+        <P>
+          Hi, there! I'm <B>{userName}</B>, a frontend designer and developer
+          living in {userLocation}. After finishing my master's degree in
+          mechanical engineering, I acquired extensive experience in a patent
+          firm and also the high-tech manufacturing industry.
+        </P>
+        <P>
+          Gaining a glimpse of user-centered methodologies from my experience in
+          the manufacturing industry, I am eager to encourage interactions among
+          people through digital products. Therefore, I've worked hard to
+          improve my coding and design skills, which allow me to create websites
+          that are intuitive and aesthetically balanced.
+        </P>
+        <P>
+          In my spare time, I like to indulge myself in <B>music</B>, broaden my
+          horizons with <B>travel</B>, document my journey by <B>photographs</B>
+          , cultivate my taste in <B>denim</B> and <B>heritage menswear</B>, and
+          feed my imagination of impossibilities with <B>video games</B> and{" "}
+          <B>films</B>.
+        </P>
+        <B>
+          You may also know more about me through my{" "}
+          <A href={linkedinInfo.url}>{linkedinInfo.label}</A> or{" "}
+          <A href={instagramInfo.url}>{instagramInfo.label}</A>.
+        </B>
+      </Section>
+      <StyledDivider yMargin="2rem" />
+      <Section>
+        <SectionTitle>Skills</SectionTitle>
+        <Grid container>
+          <TagsSection userSkills={userSkills} category={"Design"} />
+          <TagsSection userSkills={userSkills} category={"Development"} />
+          <TagsSection userSkills={userSkills} category={"General"} />
+        </Grid>
+      </Section>
+    </Container>
+  );
+}
+
+const TagsSection = ({ category, userSkills }) => {
+  const Tags = styled(Grid)`
+    padding-right: 0rem;
+    margin-bottom: 2rem;
+
+    ${MEDIA_QUERY_SM} {
+      padding-right: 1rem;
+    }
+  `;
+
+  const TagContainer = styled(Grid)`
+    padding: 0.25rem;
+  `;
+
+  const Tag = ({ tag }) => {
+    const FakedButton = styled(Button)`
+      text-transform: none;
+    `;
+
+    return (
+      <TagContainer item>
+        <FakedButton key={tag} variant="contained">
+          {tag}
+        </FakedButton>
+      </TagContainer>
+    );
+  };
+
+  return (
+    <Grid item xs={12} sm={6}>
+      <SectionSubtitle>{category}</SectionSubtitle>
+      <Tags container disableGutters>
+        {userSkills[category.toLowerCase()].map((item) => (
+          <Tag tag={item} />
+        ))}
+      </Tags>
+    </Grid>
+  );
+};
+
+export default About;
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import Layout from "../layout";
-import About from "../components/About/About";
+import About from "../components/Pages/AboutPage";
+import SEO from "../components/SEO/SEO";
 import config from "../../data/SiteConfig";
 
 function AboutPage() {
   return (
     <Layout>
-      <div className="about-container">
-        <Helmet title={`About | ${config.siteTitle}`} />
-        <About />
-      </div>
+      <Helmet title={`About | ${config.siteTitle}`} />
+      <SEO />
+      <About config={config} />
     </Layout>
   );
 }
