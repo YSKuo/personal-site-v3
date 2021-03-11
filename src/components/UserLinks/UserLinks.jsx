8726224e3f9a011c60f6a8dbfbfa02818e799b59
YSkuo <crowley3141@hotmail.com>
2021-03-11 23:55:46 +0800

    modify iconMap

--- src/components/UserLinks/UserLinks.jsx
+++ src/components/UserLinks/UserLinks.jsx
@@ -2,19 +2,7 @@ import React from "react";
 import "./UserLinks.css";
 import Link from "@material-ui/core/Link";
 import IconButton from "@material-ui/core/IconButton";
-import InstagramIcon from "@material-ui/icons/Instagram";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import GitHubIcon from "@material-ui/icons/GitHub";
-import EmailIcon from "@material-ui/icons/Email";
-import TwitterIcon from "@material-ui/icons/Twitter";
-
-const iconMapTable = {
-  Email: <EmailIcon />,
-  Twitter: <TwitterIcon />,
-  GitHub: <GitHubIcon />,
-  Instagram: <InstagramIcon />,
-  LinkedIn: <LinkedInIcon />,
-};
+import iconMapTable from "../../utils/iconMapTable";
 
 function UserLinks({ config, labeled }) {
   function getLinkElements() {
---
+++ src/utils/iconMapTable.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import InstagramIcon from "@material-ui/icons/Instagram";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import EmailIcon from "@material-ui/icons/Email";
+import TwitterIcon from "@material-ui/icons/Twitter";
+
+const iconMapTable = {
+  Email: <EmailIcon />,
+  Twitter: <TwitterIcon />,
+  GitHub: <GitHubIcon />,
+  Instagram: <InstagramIcon />,
+  LinkedIn: <LinkedInIcon />,
+};
+
+export default iconMapTable;
