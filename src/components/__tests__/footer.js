b24ecc2a1b73c1a3a24e30402bbac0d825d66317
YSkuo <crowley3141@hotmail.com>
2021-08-07 21:47:31 +0800

    footer test snapshot

--- src/components/__tests__/footer.js
+++ src/components/__tests__/footer.js
@@ -4,7 +4,7 @@ import renderer from "react-test-renderer";
 import Footer from "../Footer";
 import config from "../../../data/SiteConfig";
 
-describe("Footer", () => {
+describe("Footer snapshot", () => {
   it("render", () => {
     const tree = renderer.create(<Footer config={config} />).toJSON();
     expect(tree).toMatchSnapshot();
